49d2d6a213793e0e71019aed552619386f11b2a2
aditya <meenakshi.bhtt@gmail.com>
2025-01-12 07:48:22 +0530

    added sample wiki

--- wikiagent/app.py
+++ wikiagent/app.py
@@ -1,5 +1,5 @@
 '''
-Can we create a simple agent that will help me generate a wiki page?
+Can we create a simple agent that will help me generate a single wiki page?
 
 NOTE:
 This is a simple agent that will help you generate a wiki page, which made using simple python and langchain no fancy stuff.
@@ -7,6 +7,7 @@ This is a simple agent that will help you generate a wiki page, which made using
 import os
 from dotenv import load_dotenv
 import re
+import streamlit as st
 
 load_dotenv()
 
@@ -14,265 +15,239 @@ os.environ["GOOGLE_API_KEY"] = os.getenv("GEMINI_API_KEY")
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 
+st.title("Wiki Page Generator")
+st.write("This is a simple agent that will help you generate a wiki page using Python and Langchain.")
+
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash-latest")
 
-project_description=input("Enter the project description:")
+# Input fields
+project_description = st.text_area("Enter the project description:")
+what_you_did = st.text_area("Enter what you did:")
+what_you_learned = st.text_area("Enter what you learned:")
+what_tech_stack_used = st.text_area("Enter what tech stack you used:")
+any_special_message = st.text_area("Enter any special message:")
 
-what_you_did=input("Enter what you did:")
+did_you_use_any_urls = st.radio("Did you use any URLs?", ("yes", "no"))
 
-what_you_learned=input("Enter what you learned:")
-
-what_tech_stack_used=input("Enter what tech stack you used:")
-
-any_special_message=input("Enter any special message:")
-
-did_you_use_any_urls=input("Did you use any urls?(enter yes or no)")
-
-if did_you_use_any_urls=="yes":
-    urls=input("Enter the urls(separated by commas):")
-
+if did_you_use_any_urls == "yes":
+    urls = st.text_input("Enter the URLs (separated by commas):")
 else:
-    urls="No URL was used in this project."
+    urls = "No URL was used in this project."
 
-worker1_prompt=f"""
-You are a project description expert.
+if st.button("Generate Wiki"):
+    with st.spinner("Generating project description..."):
+        worker1_prompt = f"""
+        You are a project description expert.
 
-you are given a project description: {project_description}
+        you are given a project description: {project_description}
 
-Redraft the project description in a way that is more clear.so that any one who reads it can understand it.(Never add any other information to the project description which is not given to you)
+        Redraft the project description in a way that is more clear.so that any one who reads it can understand it.(Never add any other information to the project description which is not given to you)
 
-Always follow the XML format strictly(only output the XML format, nothing else).
+        Always follow the XML format strictly(only output the XML format, nothing else).
 
-<project_description>
-Enter here the redrafted project description.
-</project_description>
-"""
+        <project_description>
+        Enter here the redrafted project description.
+        </project_description>
+        """
 
-def extract_project_description(text):
-    return text.split('```')[1].split('<project_description>')[1].split("</project_description>")[0].strip()
+        def extract_project_description(text):
+            return text.split('```')[1].split('<project_description>')[1].split("</project_description>")[0].strip()
 
-worker1_response=llm.invoke(worker1_prompt)
+        worker1_response = llm.invoke(worker1_prompt)
+        project_description = extract_project_description(worker1_response.content)
+        st.write("Project Description:", project_description)
 
-project_description=extract_project_description(worker1_response.content)
+    with st.spinner("Generating methodology..."):
+        worker2_prompt = f"""
+        You are a Methodology expert.
 
-print(project_description)
+        You are given a project description: {project_description}
 
-worker2_prompt=f"""
-You are a Methodology expert.
+        You are also given what the developer did: {what_you_did}.(Never add any other information to the methodology which is not given to you)
 
-You are given a project description: {project_description}
+        You need to write a methodology for the project.
 
-You are also given what the developer did: {what_you_did}.(Never add any other information to the methodology which is not given to you)
+        Always follow the XML format strictly(only output the XML format, nothing else).
 
-You need to write a methodology for the project.
+        <methodology>
+        Enter here the methodology focussing on what the developer did.
+        </methodology>
+        """
 
-Always follow the XML format strictly(only output the XML format, nothing else).
+        def extract_methodology(text):
+            return text.split('```')[1].split('<methodology>')[1].split("</methodology>")[0].strip()
 
-<methodology>
-Enter here the methodology focussing on what the developer did.
-</methodology>
-"""
+        worker2_response = llm.invoke(worker2_prompt)
+        methodology = extract_methodology(worker2_response.content)
+        st.write("Methodology:", methodology)
 
-def extract_methodology(text):
-    return text.split('```')[1].split('<methodology>')[1].split("</methodology>")[0].strip()
+    with st.spinner("Generating tech stack..."):
+        worker3_prompt = f"""
+        You are a tech stack expert.
 
-worker2_response=llm.invoke(worker2_prompt)
+        You are given project description: {project_description}
 
-methodology=extract_methodology(worker2_response.content)
+        You are also given what the developer did: {what_you_did}.(Never add any other information to the tech stack which is not given to you)
 
-print(methodology)
+        You are given what tech stack the developer used: {what_tech_stack_used}
 
-worker3_prompt=f"""
-You are a tech stack expert.
+        You need to write a tech stack used for the project.
 
-You are given project description: {project_description}
+        Always follow the XML format strictly(only output the XML format, nothing else).
 
-You are also given what the developer did: {what_you_did}.(Never add any other information to the tech stack which is not given to you)
+        <tech_stack>
+        Enter here the tech stack.
+        </tech_stack>
+        """
 
-You are given what tech stack the developer used: {what_tech_stack_used}
+        def extract_tech_stack(text):
+            return text.split('```')[1].split('<tech_stack>')[1].split("</tech_stack>")[0].strip()
 
-You need to write a tech stack used for the project.
+        worker3_response = llm.invoke(worker3_prompt)
+        tech_stack = extract_tech_stack(worker3_response.content)
+        st.write("Tech Stack:", tech_stack)
 
-Always follow the XML format strictly(only output the XML format, nothing else).
+    with st.spinner("Generating other notes..."):
+        worker4_prompt = f"""
+        You are a other notes expert.
 
-<tech_stack>
-Enter here the tech stack.
-</tech_stack>
-"""
+        You are given a project description: {project_description}
 
-def extract_tech_stack(text):
-    return text.split('```')[1].split('<tech_stack>')[1].split("</tech_stack>")[0].strip()
+        You are also given what the developer did: {what_you_did}
 
-worker3_response=llm.invoke(worker3_prompt)
+        You are also given what tech stack the developer used: {what_tech_stack_used}.(Never add any other information to the other notes which is not given to you)
 
-tech_stack=extract_tech_stack(worker3_response.content)
+        You are also given any special message: {any_special_message}.(Never add any other information to the other notes which is not given to you)
 
-print(tech_stack)
+        You need to write a other notes for the project.
 
-worker4_prompt=f"""
-You are a other notes expert.
+        Always follow the XML format strictly(only output the XML format, nothing else).
 
-You are given a project description: {project_description}
+        <other_notes>
+        Enter here the other notes.
+        </other_notes>
+        """
 
-You are also given what the developer did: {what_you_did}
+        def extract_other_notes(text):
+            return text.split('```')[1].split('<other_notes>')[1].split("</other_notes>")[0].strip()
 
-You are also given what tech stack the developer used: {what_tech_stack_used}.(Never add any other information to the other notes which is not given to you)
+        worker4_response = llm.invoke(worker4_prompt)
+        other_notes = extract_other_notes(worker4_response.content)
+        st.write("Other Notes:", other_notes)
 
-You are also given any special message: {any_special_message}.(Never add any other information to the other notes which is not given to you)
+    with st.spinner("Generating mermaid chart..."):
+        worker5_prompt = f"""
+        You are a mermaid chart expert.(Never add any other information to the mermaid chart which is not given to you)
 
-You need to write a other notes for the project.
+        You are given a project description: {project_description}
 
-Always follow the XML format strictly(only output the XML format, nothing else).
+        You are also given what the developer did: {what_you_did}
 
-<other_notes>
-Enter here the other notes.
-</other_notes>
-"""
+        You are also given what tech stack the developer used: {what_tech_stack_used}.(Never add any other information to the mermaid chart which is not given to you)
 
-def extract_other_notes(text):
-    return text.split('```')[1].split('<other_notes>')[1].split("</other_notes>")[0].strip()
+        You are also given any special message: {any_special_message}.(Never add any other information to the mermaid chart which is not given to you)
 
-worker4_response=llm.invoke(worker4_prompt)
+        You need to give a mermaid chart for the project.(The project flow and the tech stack used should be clearly visible in the mermaid chart)
 
-other_notes=extract_other_notes(worker4_response.content)
+        Always follow the XML format strictly(only output the XML format, nothing else).
 
-print(other_notes)
+        <mermaid_chart>
+        Enter here the mermaid chart.
+        </mermaid_chart>
+        """
 
-worker5_prompt=f"""
-You are a mermaid chart expert.(Never add any other information to the mermaid chart which is not given to you)
+        def extract_mermaid_chart(text):
+            return text.split('```')[1].split('<mermaid_chart>')[1].split("</mermaid_chart>")[0].strip()
 
-You are given a project description: {project_description}
+        worker5_response = llm.invoke(worker5_prompt)
+        mermaid_chart = extract_mermaid_chart(worker5_response.content)
+        st.write("Mermaid Chart:", mermaid_chart)
 
-You are also given what the developer did: {what_you_did}
+    with st.spinner("Generating learning..."):
+        worker6_prompt = f"""
+        You are experrt in writing project learning.
 
-You are also given what tech stack the developer used: {what_tech_stack_used}.(Never add any other information to the mermaid chart which is not given to you)
+        You are given a project description: {project_description}
 
-You are also given any special message: {any_special_message}.(Never add any other information to the mermaid chart which is not given to you)
+        You are given what learning the developer got: {what_you_learned}.(Never add any other information to the learning which is not given to you)
 
-You need to give a mermaid chart for the project.
+        You need to write a learning for the project.
 
-Always follow the XML format strictly(only output the XML format, nothing else).
+        Always follow the XML format strictly(only output the XML format, nothing else).
 
-<mermaid_chart>
-Enter here the mermaid chart.
-</mermaid_chart>
-"""
+        <learning>
+        Enter here the learning.
+        </learning>
+        """
 
-def extract_mermaid_chart(text):
-    return text.split('```')[1].split('<mermaid_chart>')[1].split("</mermaid_chart>")[0].strip()
+        def extract_learning(text):
+            return text.split('```')[1].split('<learning>')[1].split("</learning>")[0].strip()
 
-worker5_response=llm.invoke(worker5_prompt)
+        worker6_response = llm.invoke(worker6_prompt)
+        learning = extract_learning(worker6_response.content)
+        st.write("Learning:", learning)
 
-mermaid_chart=extract_mermaid_chart(worker5_response.content)
+    with st.spinner("Generating final markdown..."):
+        main_prompt = f"""
+        You are a md file expert.
+        You need to generate a md file for the project.
 
-print(mermaid_chart)
+        Project Description: {project_description}
 
-worker6_prompt=f"""
-You are experrt in writing project learning.
+        Methodology: {methodology}
 
-You are given a project description: {project_description}
+        Tech Stack: {tech_stack}
 
-You are given what learning the developer got: {what_you_learned}.(Never add any other information to the learning which is not given to you)
+        Other Notes: {other_notes}
 
-You need to write a learning for the project.
+        Mermaid Chart: {mermaid_chart}
 
-Always follow the XML format strictly(only output the XML format, nothing else).
+        Urls: {urls}
 
-<learning>
-Enter here the learning.
-</learning>
-"""
+        Learning: {learning}
 
-def extract_learning(text):
-    return text.split('```')[1].split('<learning>')[1].split("</learning>")[0].strip()
+        (Never add any other information to the md file which is not given to you)
 
-worker6_response=llm.invoke(worker6_prompt)
+        You need to generate a md file for the project.(Make sure to add following to the md file: Project Description, Methodology, Tech Stack, Other Notes, Mermaid Chart, Urls(If any), and Learning)
 
-learning=extract_learning(worker6_response.content)
+        Always follow the XML format strictly(only output the XML format, nothing else).
 
-print(learning)
+        <md_file>
+        Enter here the md file.
+        </md_file>
+        """
 
-main_prompt=f"""
-You are a md file expert.
-You need to generate a md file for the project.
+        def extract_md_file(text):
+            """
+            Extracts content between <md_file> tags from a text string, handling nested code blocks
+            including Mermaid charts.
+            """
+            import re
+            
+            clean_text = text.strip('`')
+            if clean_text.startswith('xml'):
+                clean_text = clean_text[3:].lstrip()
+                
+            pattern = r'<md_file>([\s\S]*?)</md_file>'
+            match = re.search(pattern, clean_text)
+            
+            if not match:
+                raise ValueError("No <md_file> tags found in the input text")
+                
+            content = match.group(1)
+            return content.strip()
 
-Project Description: {project_description}
-
-Methodology: {methodology}
-
-Tech Stack: {tech_stack}
-
-Other Notes: {other_notes}
-
-Mermaid Chart: {mermaid_chart}
-
-Urls: {urls}
-
-Learning: {learning}
-
-(Never add any other information to the md file which is not given to you)
-
-You need to generate a md file for the project.(Make sure to add following to the md file: Project Description, Methodology, Tech Stack, Other Notes, Mermaid Chart, Urls(If any), and Learning)
-
-Always follow the XML format strictly(only output the XML format, nothing else).
-
-<md_file>
-Enter here the md file.
-</md_file>
-""" 
-
-def extract_md_file(text):
-    """
-    Extracts content between <md_file> tags from a text string, handling nested code blocks
-    including Mermaid charts.
-    
-    Args:
-        text (str): Input text containing markdown content wrapped in <md_file> tags
+        worker6_response = llm.invoke(main_prompt)
+        md_file = extract_md_file(worker6_response.content)
         
-    Returns:
-        str: Extracted content between <md_file> tags, with proper handling of nested code blocks
+        st.markdown("## Generated Wiki")
+        st.markdown(md_file)
         
-    Example:
-        >>> text = '''```xml
-        ... <md_file>
-        ... # Header
-        ... ```mermaid
-        ... graph LR
-        ... A --> B
-        ... ```
-        ... </md_file>
-        ... ```'''
-        >>> print(extract_md_file(text))
-        # Header
-        ```mermaid
-        graph LR
-        A --> B
-        ```
-    """
-    import re
-    
-    # First, remove the outer code block markers if present
-    clean_text = text.strip('`')
-    if clean_text.startswith('xml'):
-        clean_text = clean_text[3:].lstrip()
-        
-    # Extract content between md_file tags
-    pattern = r'<md_file>([\s\S]*?)</md_file>'
-    match = re.search(pattern, clean_text)
-    
-    if not match:
-        raise ValueError("No <md_file> tags found in the input text")
-        
-    content = match.group(1)
-    return content.strip()
-
-worker6_response=llm.invoke(main_prompt)
-print(worker6_response.content)
-
-md_file=extract_md_file(worker6_response.content)
-
-print(md_file)
-
-with open("wiki.md","w") as f:
-    f.write(md_file)
-
+        # Save to file and trigger download
+        st.download_button(
+            label="Download Wiki",
+            data=md_file,
+            file_name="wiki.md",
+            mime="text/markdown"
+        )
+        st.success("Wiki has been generated! Click above to download.")
